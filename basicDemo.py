c1330702227ce0a3383a4189a592daea80914a1d
yaoqijun <yaoqijunmail@foxmail.com>
2016-07-25 09:41:47 +0800

    fix

--- basicDemo.py
+++ basicDemo.py
@@ -11,30 +11,30 @@ data = {'kind': 'pyspark',
 	"driverMemory":"512m"}
 url = host + '/sessions'
 
-r = requests.post(host + '/sessions', data=json.dumps(data), headers=headers)
-pprint.pprint(r.json)
-pprint.pprint(r.headers['Location'])
+# r = requests.post(host + '/sessions', data=json.dumps(data), headers=headers)
+# pprint.pprint(r.json)
+# pprint.pprint(r.headers['Location'])
 
 
-session_url = host + '/sessions/0'
+session_url = host + '/sessions/3'
 # r = requests.get(session_url, headers=headers)
 # pprint.pprint(r.json())
 
-# statements_url = session_url + '/statements'
-# data = {
-#   'code': textwrap.dedent("""
-#     import random
-#     NUM_SAMPLES = 100000
-#     def sample(p):
-#       x, y = random.random(), random.random()
-#       return 1 if x*x + y*y < 1 else 0
+statements_url = session_url + '/statements'
+data = {
+  'code': textwrap.dedent("""
+    import random
+    NUM_SAMPLES = 100000
+    def sample(p):
+      x, y = random.random(), random.random()
+      return 1 if x*x + y*y < 1 else 0
 
-#     count = sc.parallelize(xrange(0, NUM_SAMPLES)).map(sample).reduce(lambda a, b: a + b)
-#     print "Pi is roughly %f" % (4.0 * count / NUM_SAMPLES)
-#     """)
-# }
-# r = requests.post(statements_url, data=json.dumps(data), headers=headers)
-# pprint.pprint(r.json())
+    count = sc.parallelize(xrange(0, NUM_SAMPLES)).map(sample).reduce(lambda a, b: a + b)
+    print "Pi is roughly %f" % (4.0 * count / NUM_SAMPLES)
+    """)
+}
+r = requests.post(statements_url, data=json.dumps(data), headers=headers)
+pprint.pprint(r.json())
 
 # statement_url = host + '/sessions/0/statements/0'
 # r = requests.get(statement_url, headers=headers)
